7315a037d0de90e81f54db219d1bb1aff5927b5d
Christina O'Donnell <cdo@mutix.org>
2025-11-02 19:17:01 +0000

    Setup flake.

---
+++ Test/Test1.agda
@@ -0,0 +1,5 @@
+module Test.Test1 where
+open import Cubical.Foundations.Prelude
+
+data X : Type where
+  f : X
--- nix/args.nix
+++ nix/args.nix
@@ -9,6 +9,17 @@
       _module.args = {
         pkgs = import inputs.nixpkgs {
           inherit system;
+          overlays = [
+            (final: prev: {
+              agdaPackages = prev.agdaPackages // {
+                cubical = final.agdaPackages.mkDerivation {
+                  pname = "cubical";
+                  version = "flake";
+                  src = inputs.agda-cubical;
+                };
+              };
+            })
+          ];
         };
       };
     };
