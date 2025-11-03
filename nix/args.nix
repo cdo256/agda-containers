cb9eb4f6b39bcd0a4b51b8bfa095e763f35cd085
Christina O'Donnell <cdo@mutix.org>
2025-11-03 01:41:16 +0000

    Add build phase to flakes

--- nix/args.nix
+++ nix/args.nix
@@ -16,14 +16,34 @@
                   version = "9.0.0";
                   src = inputs.agda-cubical;
                   sha256 = "";
+                  buildInputs = [
+                    prev.ghc
+                    prev.agda
+                  ];
                   buildPhase = ''
-                    echo foo
+                    cp -r $src $TMPDIR/src
+                    chmod -R +w $TMPDIR/src
+                    cd $TMPDIR/src
+                    runhaskell Everythings.hs gen-except Codata Papers
+                    agda Cubical/README.agda
+                  '';
+                  installPhase = ''
+                    cp -r $TMPDIR/src $out
                   '';
                 });
                 agda-categories = prev.agdaPackages.agda-categories.overrideAttrs (old: {
                   version = "9.0.0";
                   src = inputs.agda-categories;
                   sha256 = "";
+                  buildPhase = ''
+                    cp -r $src $TMPDIR/src
+                    chmod -R +w $TMPDIR/src
+                    cd $TMPDIR/src
+                    agda --build-library
+                  '';
+                  installPhase = ''
+                    cp -r $TMPDIR/src $out
+                  '';
                 });
               };
             })
